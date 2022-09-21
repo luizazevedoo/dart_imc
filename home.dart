eb8540371387f247f2e03d63c9f414547fa0d9cd
Lu Azevedo <47855767+luizazevedoo@users.noreply.github.com>
2022-09-21 13:53:08 -0300

    Add files via upload

--- home.dart
+++ home.dart
@@ -44,13 +44,13 @@ class _HomeState extends State<Home> {
               TextFormField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: "Peso (kg)",
-                  labelStyle: TextStyle(color: Colors.blueAccent)),
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.blueAccent)),
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
                 controller: pesoController,
                 validator: (value) {
-                  if (value!.isEmpty){
+                  if (value!.isEmpty) {
                     return "Insira seu peso!";
                   } else {
                     return null;
@@ -60,15 +60,73 @@ class _HomeState extends State<Home> {
               TextFormField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: "Altura (m)",
-                  labelStyle: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
-                textAlign
-                ),
-              )
-            ]
-          )
-        )
-      )
-    )
+                    labelText: "Altura (m)",
+                    labelStyle:
+                        TextStyle(color: Colors.blueAccent, fontSize: 25.0)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+                controller: alturaController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Insira sua altura!";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _calcular();
+                      }
+                    },
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold)),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _calcular() {
+    String _texto = "";
+    String _imagem = "";
+
+    double peso = double.parse(pesoController.text);
+    double altura = double.parse(alturaController.text);
+
+    double imc = peso / (altura * altura);
+
+    if (imc < 18.6) {
+      _texto = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/thin.png";
+    } else if (imc >= 18.6 && imc < 24.9) {
+      _texto = "Peso ideal (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/average.png";
+    } else if (imc >= 24.9 && imc < 29.9) {
+      _texto = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/over.png";
+    } else if (imc >= 29.9 && imc < 34.9) {
+      _texto = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    } else if (imc >= 34.9 && imc < 39.9) {
+      _texto = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    } else if (imc >= 39.9) {
+      _texto = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    }
+
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => Result(_imagem, _texto)));
   }
 }
